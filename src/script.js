d5aba37cb8f5d71195024e94f1460201fd83c275
Briso10-dev <kouambrice10@gmail.com>
2025-01-19 12:25:12 +0100

    Project finish/standby for now

--- src/script.js
+++ src/script.js
@@ -5,12 +5,13 @@ const close = document.getElementById("close");
 
 // Toggle mobile menu visibility
 burger.addEventListener("click", () => {
-    mobileNav.classList.remove("hidden");
-    mobileNav.classList.remove("-translate-x-full"); // Slide in
+    mobileNav.classList.remove("hidden"); //Step 1 : Make visible the nav-bar
+    mobileNav.classList.remove("-translate-x-full"); // Step 2 : Removing make that it starts being visible
+    mobileNav.classList.add("translate-x-0");  //Brings the navbar into view.
   });
   
 // Hide the mobile nav with slide-out effect
 close.addEventListener("click", () => {
-  mobileNav.classList.add("-translate-x-full"); // Slide out
-  mobileNav.classList.remove("translate-x-0");
+  mobileNav.classList.remove("translate-x-0"); //Brings the navbar reback into view.
+  mobileNav.classList.add("-translate-x-full"); // step 1 : When clicking the close button,adding it brings back to off view of its total with negatively of the nav
 });
