d4b7ffe859920fa2fad73269800255c9db4f9964
Briso10-dev <kouambrice10@gmail.com>
2025-01-19 08:03:10 +0100

    NavBar responsive JS

--- src/script.js
+++ src/script.js
@@ -6,9 +6,11 @@ const close = document.getElementById("close");
 // Toggle mobile menu visibility
 burger.addEventListener("click", () => {
     mobileNav.classList.remove("hidden");
+    mobileNav.classList.remove("-translate-x-full"); // Slide in
   });
   
-  close.addEventListener("click", () => {
-    mobileNav.classList.add("hidden");
-  })
- 
+// Hide the mobile nav with slide-out effect
+close.addEventListener("click", () => {
+  mobileNav.classList.add("-translate-x-full"); // Slide out
+  mobileNav.classList.remove("translate-x-0");
+});
